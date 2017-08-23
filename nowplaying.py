03765b28394b65f5f7e8a6d8b55b4c7b22f0da36
Colin Johnson <colinj@gmail.com>
2017-08-23 01:06:23 -0400

    spelling counts

--- nowplaying.py
+++ nowplaying.py
@@ -138,7 +138,7 @@ class NowPlayingApp(App):
     def build(self):
         npb = NowPlayingBox()
         npb.nowplaying.start_update()
-        Clock.schedule_interval(nbp.albumart.reload())
+        Clock.schedule_interval(npb.albumart.reload())
         return npb
 
 
